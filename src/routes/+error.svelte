81b2a7477182a91ca2fc43605c40c45b63a93c24
JustCallMeSimon <github@jcms.dev>
2024-02-08 14:11:27 +0100

    best 404 ever

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,26 +6,30 @@
 	
 	let randomCatImage = "";
    
-	onMount(async () => {
+	async function getImage() {
 	  try {
-		const response = await fetch("https://api.thecatapi.com/v1/images/search?breed_ids=black");
+		const response = await fetch("https://api.thecatapi.com/v1/images/search");
 		const data = await response.json();
 		randomCatImage = data[0].url;
 	  } catch (error) {
 		console.error("Error fetching cat image (dont care lol | show my logo ig)", error);
 	  }
-	});
+	};
   </script>
 
 <div class="content">
 	<h1 class="title text">{$page.status} - {$page.error.message}</h1>
-	{#if randomCatImage}
-		<h2 class="description text">This is not a valid url but... take this cat!</h2>
-		<img src={randomCatImage} alt="A (probably) really cute cat :3" />
-	{:else}
-		<h2 class="description text">This is not a valid url and... id show you a cute cat but that didnt work :c</h2>
-		<img class="img404 ctm-border" src="{base}/logo.svg" alt="JCMS" title="just my logo :c (take at least this cat: 🐈)" />
-	{/if}
+	{#await getImage()}
+		<h2 style="color:white">Looking for a cute cat...</h2>
+	{:then _} 
+		{#if randomCatImage}
+			<h2 class="description text">This is not a valid url but... take this cat!</h2>
+			<img class="img404 ctm-border" src={randomCatImage} alt="A (probably) really cute cat :3" />
+		{:else}
+			<h2 class="description text">This is not a valid url and... id show you a cute cat but that didnt work :c</h2>
+			<img class="img404 ctm-border" src="{base}/logo.svg" alt="JCMS" title="just my logo :c (take at least this cat: 🐈)" />
+		{/if}
+	{/await}
 	<BackButton scale="0.85"/>
 </div>
 
@@ -45,12 +49,20 @@
 	}
 	
 	.img404 {
-		padding: 2vw;
-		max-width: 20%;
-		aspect-ratio: 1;
-		background-size: contain;
+		border-color: black;
+		max-width: 80%;
+		max-height: 30%;
+		min-height: 0px;
+		transition: 0.5s ease-out;
+	}
+	
+	.img404:hover {
+		border-color: white;
+		min-height: 96vh;
+		max-width: 90vw;
+		padding: 1vh;
+		z-index: 5;
+		transform: translateY(-3.5vh);
 	}
 	
 </style>
-  
-  
